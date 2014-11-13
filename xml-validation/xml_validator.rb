f4c71ba2bccce32c226ca3ff265a49028eb03b8f
Chris C Cerami <chrisccerami@gmail.com>
2014-11-13 09:08:16 -0500

    Planning xml-validation.rb

---
+++ xml-validation/xml_validator.rb
@@ -0,0 +1,8 @@
+# check for opening items using scan or regex
+# store opening items in an array
+# check for closing items using scan or regex
+# each closing item should correspond with the last opening item in the array
+# if a correponding opening item exists for the closing item, remove it from array
+# if array is empty at the end, all items are closed
+# make an exception for self-closing items and doctype items
+# only one root element (doctype element?) should exist
