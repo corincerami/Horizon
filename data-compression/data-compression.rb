692741d9878e63d7ee62c483f7c9cfca893ba49a
Chris C Cerami <chrisccerami@gmail.com>
2014-11-12 21:59:56 -0500

    UTF-8 Encoding working, compression and uncompression lossless

--- data-compression/data-compression.rb
+++ data-compression/data-compression.rb
@@ -1,17 +1,18 @@
 require "pry"
 
-def common_words(file)
-  word_count = Hash.new(0)
-  File.read(file).scan(/\w+/).each do |word|
-    if word.length >= 2
-      word_count[word.downcase] += 1
+def common_words(file) # builds a hash from file
+  word_count = Hash.new(0) # default value for hash is 0
+  File.read(file).scan(/\w+/).each do |word| # returns an array
+    if word.length >= 2 # ignore words with 1 character
+      word_count[word.downcase] += 1 # increase the count in the hash
     end
   end
+  # saves a hash of the most common words
   most_common = word_count.sort_by { |_, count| count }.reverse.first(132)
   compression_hash = {}
-  i = 124
-  most_common.each do |word, count|
-    #i = 0 if i == 256
+  i = 124 # starting with UTF-8 124 because 1-123 are letters/whitespace
+  most_common.each do |word, _| # for each of the most common words
+    # uses UTF-8 chars not ASCII to facilitate uncompression
     compression_hash[i.chr(Encoding::UTF_8)] = word
     i += 1
   end
@@ -20,45 +21,45 @@ end
 
 def compress(file)
   original_size = File.read(file).size
-  compression_hash = common_words(file)
-  text = File.read(file)
-  compression_hash.each do |char, word|
-    text.gsub!(word, char)
+  compression_hash = common_words(file) # grabs hash from common_words method
+  text = File.read(file) # saves entire file as a string
+  compression_hash.each do |char, word| # for each pair in compression_hash
+    text.gsub!(word, char) # replace word in file with UTF-8 character
   end
-  File.open("c-#{file}", "w") { |f| f.write(text) }
+  File.open("c-#{file}", "w") { |f| f.write(text) } # writes a new file
+  # to preserve the original file, in case of compression errors
   final_size = File.read("c-#{file}").size.to_f
-  rate = final_size / original_size * 100
+  final_size / original_size * 100 # returns compression rate
 end
 
-def uncompress(file)
-  text = File.read("c-#{file}")
-  compression_hash = common_words(file)
-  compression_hash.each do |char, word|
-    char = char.to_s
-    text.gsub!(char, word)
+def uncompress(file) # takes in input from ARGV
+  text = File.read("c-#{file}") # saves entire file as a string
+  compression_hash = common_words(file) # grabs hash from common_words method
+  compression_hash.each do |char, word| # for each pair in compression_hash
+    text.gsub!(char, word) # replaces UTF-8 char in text with word
   end
-  File.open("u-#{file}", "w") { |f| f.write(text) }
+  File.open("u-#{file}", "w") { |f| f.write(text) } # saved text to a new file
   original = File.read(file)
   final = File.read("u-#{file}")
-  original == final
+  original == final # checks whether compression was lossless
 end
 
+# captures the filename and either (-c)ompress or (-u) uncompress
 file = ARGV[1]
 option = ARGV[0]
 
-
 if option == "-c"
-  # compresses the file
+  # runs the compression method and outputs a message and compression rate
   compression_rate = compress(file)
   puts "File #{file} was compressed into c-#{file}."
   puts "Compression rate of #{compression_rate.round(2)}"
 elsif option == "-u"
-  # uncompresses the file
+  # runs uncompression file and out a message and checks for losslessness
   uncompressed_file = uncompress(file)
   puts "File c-#{file} uncompressed into u-#{file}."
   puts "Lossless status is #{uncompressed_file}."
 else
-  # asks the user to enter the command in the proper format
+  # asks the user to re-enter the command in the proper format
   puts "Please enter in the following format:"
   puts "ruby data-compression.rb -c file"
   puts "or"
