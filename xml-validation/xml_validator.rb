b1703c4ced41e18d486cf527af7f0ae43a8785f8
Chris C Cerami <chrisccerami@gmail.com>
2014-11-15 13:18:56 -0500

    XML Validation working for tags/tag order

--- xml-validation/xml_validator.rb
+++ xml-validation/xml_validator.rb
@@ -1,22 +1,26 @@
-require "pry"
-
 input = ARGV[0]
 
-# checks for missing tags, ignores self-closing tags
-# doesn't account for tags in the wrong order
-open_tags = File.read(input).scan(/<\w+>/)
-close_tags = File.read(input).scan(/<\/\w+>/)
+# finds all tags in the xml file being passed as input
+all_tags = File.read(input).scan(/<\/*\w+\s{0,1}\w*\=*\"*\w*\"*>/)
 
-open_tags.each do |tag|
-  if close_tags.include?(tag.insert(1, "/"))
-    open_tags.delete_at(open_tags.index(tag))
-    close_tags.delete_at(close_tags.index(tag))
-  else
-    puts "INVALID"
-    exit
+def check_tags(all_tags)
+  # creates an array to use as a stack
+  tags_stack = Array.new
+  all_tags.each do |tag|
+    #tag_index = tags_stack.index(tag)
+    if tag[1] == "/"
+      tag.slice!(1) # remove / character
+      tag.slice!(-1) # remove > character
+      # if last tag in stack matches closing tag, delete it
+      if tags_stack[-1].start_with?(tag) # closing tag matches last open tag
+        tags_stack.pop # delete last tag in the stack
+      end
+    else
+      tags_stack << tag # add open tag to the stack
+    end
   end
+  tags_stack
 end
 
-# checks for root element
-
-puts "VALID"
+# if the stack is empty, all tags were closed in the right order, else invalid
+check_tags(all_tags).empty? ? puts("VALID") : puts("INVALID")
