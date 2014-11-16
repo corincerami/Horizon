88bdafbb5a0506ce4792568e9da7fe10bbe8cf05
Chris C Cerami <chrisccerami@gmail.com>
2014-11-15 19:27:10 -0500

    XML-Validator now checks for only one root

--- xml-validation/xml_validator.rb
+++ xml-validation/xml_validator.rb
@@ -1,9 +1,8 @@
 input = ARGV[0]
 
-# finds all tags in the xml file being passed as input
-all_tags = File.read(input).scan(/<\/*\w+\s{0,1}\w*\=*\"*\w*\"*>/)
-
-def check_tags(all_tags)
+def check_tags(input)
+  # finds all tags in the xml file bei
+  all_tags = File.read(input).scan(/<\/*\w+\s{0,1}\w*\=*\"*\w*\"*>/)
   # creates an array to use as a stack
   tags_stack = Array.new
   all_tags.each do |tag|
@@ -19,8 +18,14 @@ def check_tags(all_tags)
       tags_stack << tag # add open tag to the stack
     end
   end
-  tags_stack
+  tags_stack.empty?
+end
+
+#check for one and only one root element
+def check_root(input)
+  root = File.read(input).scan(/<\?\w+\sversion=/)
+  root.length == 1
 end
 
 # if the stack is empty, all tags were closed in the right order, else invalid
-check_tags(all_tags).empty? ? puts("VALID") : puts("INVALID")
+check_tags(input) && check_root(input) ? puts("VALID") : puts("INVALID")
