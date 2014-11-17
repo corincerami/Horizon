41116d8cbd5bb0892c7a61d455d39e2babebc009
Chris C Cerami <chrisccerami@gmail.com>
2014-11-17 17:24:43 -0500

    Got data-compression down to ~50%

--- data-compression/data-compression.rb
+++ data-compression/data-compression.rb
@@ -7,8 +7,8 @@ option = ARGV[0]
 def count_words(file)
   word_count = Hash.new(0) # default value for hash is 0
   File.read(file).scan(/\w+/).each do |word| # returns an array
-    if word.length >= 2 # ignore words with 1 character
-      word_count[word.downcase] += 1 # increase the count in the hash
+    if word.length >= 3 # ignore words with 1 character
+      word_count[word] += 1 # increase the count in the hash
     end
   end
   word_count
@@ -18,7 +18,8 @@ word_count = count_words(file)
 
 def common_words(file, word_count) # builds a hash from file
   # saves a hash of the most common words
-  most_common = word_count.sort_by { |_, count| count }.reverse.first(132)
+  number = word_count.length
+  most_common = word_count.sort_by { |_, count| count }.reverse.first(number)
   compression_hash = {}
   i = 124 # starting with UTF-8 124 because 1-123 are letters/whitespace
   most_common.each do |word, _| # for each of the most common words
@@ -58,7 +59,7 @@ if option == "-c"
   # runs the compression method and outputs a message and compression rate
   compression_rate = compress(file, word_count)
   puts "File #{file} was compressed into c-#{file}."
-  puts "Compression rate of #{compression_rate.round(2)}"
+  puts "File is now #{compression_rate.round(2)} of its original size"
 elsif option == "-u"
   # runs uncompression file and out a message and checks for losslessness
   uncompressed_file = uncompress(file, word_count)
--- word-counter/word_counter.rb
+++ word-counter/word_counter.rb
@@ -1,15 +1,24 @@
 file = ARGV[0]
 number = ARGV[1].to_i
 stop_words = File.read("stop_words.txt")
-word_hash = Hash.new(0)
 
-File.read(file).scan(/\w+/).each do |word|
-  unless stop_words.include?(word.downcase)
-    word_hash[word.downcase] += 1
+def count_words(file, stop_words)
+  word_hash = Hash.new(0)
+  File.read(file).scan(/\w+/).each do |word|
+    unless stop_words.include?(word.downcase)
+      word_hash[word.downcase] += 1
+    end
+  end
+  word_hash
+end
+
+word_hash = count_words(file, stop_words)
+
+def print_words(word_hash, number)
+  word_hash = word_hash.sort_by { |_, count| -count }
+  word_hash.first(number).each do |word, count|
+    puts "#{word}: #{count}"
   end
 end
 
-word_hash = word_hash.sort_by { |_, count| -count }
-word_hash.first(number).each do |word, count|
-  puts "#{word}: #{count}"
-end
+print_words(word_hash, number)
