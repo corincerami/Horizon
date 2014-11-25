d997105f283ade1a9d13e98513e8594c34900e4b
Chris C Cerami <chrisccerami@gmail.com>
2014-11-25 09:44:22 -0500

    Movies and Actors now link to one another

--- movie-catalog-deluxe/movies.rb
+++ movie-catalog-deluxe/movies.rb
@@ -30,13 +30,13 @@ get "/actors" do
   erb :'actors/index'
 end
 
-get "/actors/:actor_name" do
-  actor_name = params[:actor_name]
-  query = "SELECT movies.title, cast_members.character, actors.name
+get "/actors/:id" do
+  actor_id = params[:id]
+  query = "SELECT movies.title, movies.id, cast_members.character, actors.name
            FROM actors
-           JOIN cast_members ON actors.id = cast_members.actor_id
-           JOIN movies ON movies.id = cast_members.movie_id
-           WHERE actors.name = '#{actor_name}';"
+           LEFT OUTER JOIN cast_members ON actors.id = cast_members.actor_id
+           LEFT OUTER JOIN movies ON movies.id = cast_members.movie_id
+           WHERE actors.id = '#{actor_id}';"
   @actor_info = select_from_db(query)
   erb :'actors/show'
 end
@@ -47,16 +47,16 @@ get "/movies" do
   erb :'movies/index'
 end
 
-get "/movies/:title" do
-  movie_title = params[:title]
+get "/movies/:id" do
+  movie_id = params[:id]
   query = "SELECT genres.name AS genre, studios.name AS studio,
-           actors.name AS actor, cast_members.character, movies.title AS title
+           actors.name AS actor, actors.id AS actor_id, cast_members.character, movies.title AS title
            FROM movies
            JOIN cast_members ON movies.id = cast_members.movie_id
            JOIN actors ON actors.id = cast_members.actor_id
            JOIN genres ON genres.id = movies.genre_id
-           JOIN studios ON studios.id = movies.studio_id
-           WHERE movies.title = '#{movie_title}';"
+           LEFT OUTER JOIN studios ON studios.id = movies.studio_id
+           WHERE movies.id = '#{movie_id}';"
   @movie_info = select_from_db(query)
   erb :'movies/show'
 end
