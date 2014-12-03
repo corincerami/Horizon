81ff64ba3fe1d696e49dec55e6db5b389ce8be04
Chris C Cerami <chrisccerami@gmail.com>
2014-12-03 08:30:40 -0500

    Commented changes from last commit

--- blackjack/hand.rb
+++ blackjack/hand.rb
@@ -5,6 +5,9 @@ class Hand
     calculate_value
   end
 
+  # i took out the argument from this method so it just calculates the
+  # total score, so it's calculating the score for the whole hand every
+  # time you're dealt a new card
   def calculate_value
     @score = 0
     @cards.each do |card|
@@ -14,11 +17,15 @@ class Hand
         @score += card.number.to_i
       end
     end
+    # i moved the called to ace_value out of the each loop, so that aces
+    # would always be tallied last.
     @cards.each do |card|
       aces_value(card) if card.ace?
     end
   end
 
+  # this method and the one above now add directly to score rather than
+  # returning a value for each card.
   def aces_value(card)
     if @score > 10
       @score += 1
